5c7b7d6e204e84642f7976746492e441b6b56349
Florian Schäfer <florian.schaefer@securepoint.de>
2014-01-29 09:36:30 +0100

    Add md5 command.

---
+++ jim-md5.c
@@ -0,0 +1,75 @@
+/*
+ * Jim - md5 command
+ *
+ * Copyright 2014 Florian Schäfer <florian.schaefer@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE JIM TCL PROJECT ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * JIM TCL PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * official policies, either expressed or implied, of the Jim Tcl Project.
+ */
+
+#include <string.h>
+#include <openssl/md5.h>
+#include <jim.h>
+
+static int
+MD5_Cmd(Jim_Interp *interp, int argc, Jim_Obj *const *argv)
+{
+
+    if (argc != 2) {
+        Jim_WrongNumArgs(interp, 1, argv, "value");
+        return JIM_ERR;
+    }
+
+    const char *string;
+    string = Jim_String(argv[1]);
+
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, string, strlen(string));
+
+    unsigned char digest[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    MD5_Final(digest, &ctx);
+
+    char md5[33];
+    int i;
+    for (i = 0; i < 16; ++i)
+        sprintf(&md5[i*2], "%02x", (unsigned int)digest[i]);
+
+    Jim_SetResultString(interp, md5, -1);
+    return JIM_OK;
+}
+
+int
+Jim_md5Init(Jim_Interp *interp)
+{
+    if (Jim_PackageProvide(interp, "md5", "0.1", JIM_ERRMSG))
+        return JIM_ERR;
+
+    Jim_CreateCommand(interp, "md5", MD5_Cmd, NULL, NULL);
+    return JIM_OK;
+}
